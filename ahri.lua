4636cdb2b0eb7ea736fbcb163a5a7c4a3d9d217f
Andgel <andgel.halley@epitech.eu>
2019-05-12 10:11:17 +0200

    Fixed update command

--- ahri.lua
+++ ahri.lua
@@ -7308,7 +7308,7 @@ function updateBot(msg)
 	})
 	local file = io.popen("git pull 2>&1")
 	local output = table.concat(arrayFromIt(file:lines()), "\n")
-	local success, _ err_code = file:close()
+	local success, _, err_code = file:close()
 
 	if not success then
 		message:setEmbed({
